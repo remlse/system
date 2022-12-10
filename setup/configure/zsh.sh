b6b3efce4564438b7c13417cc0bd9b36ff0b89f9
Remo Senekowitsch <contact@remlse.dev>
2022-12-10 08:43:30 +0100

    Persist HEADLESS env variable on system

--- setup/configure/zsh.sh
+++ setup/configure/zsh.sh
@@ -5,6 +5,12 @@ echo "Making sure zsh is setup..."
 line="[[ -o rcs ]] && export ZDOTDIR=\$HOME/.config/zsh"
 if ! grep "$line" /etc/zshenv &> /dev/null ; then
     echo "$line" | sudo tee -a /etc/zshenv > /dev/null
+
+    # ensure HEADLESS is set forever on that system
+    if ! [ -z ${HEADLESS+x} ]
+    then
+        echo "HEADLESS=true" | sudo tee -a /etc/zshenv > /dev/null
+    fi
 fi
 
 sudo chsh -s $(which zsh) $(whoami) &> /dev/null
